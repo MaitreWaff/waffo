5cf0886332c8487c45686dc4f6cfd87e794718ab
MaitreWaff <waffoluc@gmail.com>
2017-08-16 05:10:58 +0100

    Personalisation de l Acceuil du Blog. Suite.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,5 @@
+from time import time
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -7,10 +9,15 @@ from django.dispatch import receiver
 CF_LOC_MAX      = 30
 TF_BIO_MAX_L    = 500.
 
+# Retourne le Nom De l'Image telechargee.
+def get_upload_file_name(instance, filename):
+    return "uploaded_files/profiles/%s_%s" % (str(time()).replace('.', '_'), filename)
+
 # Create your models here.
 
 class Profile(models.Model):
     user        = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo       = models.FileField(upload_to=get_upload_file_name, blank=True)
     bio         = models.TextField(max_length=TF_BIO_MAX_L, blank=True)
     location    = models.CharField(max_length=CF_LOC_MAX, blank=True)
     date_naiss  = models.DateField(null=True, blank=True)
