e5c85324e5dbc085d4b2c3744cf834659de882b6
MaitreWaff <waffoluc@gmail.com>
2017-08-23 01:58:21 +0100

    Ajout de Humanize pour le Profil Utilisateur.

--- waffo/settings.py
+++ waffo/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     # 'bootstrap3',
     # 'django_forms_bootstrap',
 )
