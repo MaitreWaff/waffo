7f885765cb0932d7e1ff48351576209c9f0ba32b
MaitreWaff <waffoluc@gmail.com>
2017-08-24 19:52:56 +0100

    Debut ecriture des Tests. Debut tests des Models.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import Blog
+from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+
+# models test
+class BlogTest(TestCase):
+
+    def create_blog(self, profile, theme='Premier Theme du Blog.'):
+        blg = Blog.objects.create(auteur=profile, theme=theme)
+        return blg
+
+    def test_blog_creation(self, ):
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import Profile
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
 
 # Create your tests here.
+
+# models test
+class ProfileTest(TestCase):
+
+    def create_profile(self, username="waluc", email="waffoluc@yahoo.com"):
+        user = User.objects.create(username=username, email=email)
+        pf = Profile.objects.get(user=user)
+        pf.location = 'Bertoua'
+        pf.save()
+        return pf
+
+    def test_profile_creation(self):
+        pf = self.create_profile()
+        self.assertTrue(isinstance(pf, Profile))
+        self.assertEqual(pf.__unicode__(), pf.user.username)
+        self.assertEqual(pf.location, 'Bertoua')
+
+
