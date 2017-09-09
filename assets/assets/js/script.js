4257dec3f2eef277c1135878a41bfde6415a4e14
MaitreWaff <waffoluc@gmail.com>
2017-09-10 00:48:10 +0100

    Affinage Position centre page.

--- assets/assets/js/script.js
+++ assets/assets/js/script.js
@@ -22,13 +22,86 @@ $(document).ready(function(){
 ////        $('div.text-muted').hide();
 ////        $('div.text-muted').css('background-color', '#CCCCCC');
 //    });
+//
+//    $('.update div.card-body p').hover(function(){
+//        $(this).addClass('zebraHover');
+////        alert(this);
+//    }, function(){
+//        $(this).removeClass('zebraHover');
+////        alert('mouseout');
+//    });
 
-    $('.update').hover(function(){
-        $(this).addClass('zebraHover');
-//        alert(this);
-    }, function(){
-        $(this).removeClass('zebraHover');
-//        alert('mouseout');
+    $('p').animate({
+        padding : '1px',
+        fontSize: '10px',
+        backgroundColor: '#AFEEEE',
+    }, 6000);
+
+//
+//    $('#navbarResponsive li').hover(function(){
+//        $(this).animate({ fontSize: '+=35px'}, 3000, 'linear');
+//    },function(){
+//        $(this).animate({ fontSize: '-=35px'}, 3000, 'linear');
+//    });
+//
+
+
+    $('p.m-0').animate({ opacity: 'hide', height: 'hide'}, 10000);
+
+    $('.update').animate({'backgroundColor': '#F8F8FF'}, 3000);
+
+
+
+    $('.update:first').click(function(){
+        $(this).hide().delay(2000).slideDown(6000).delay(3000).fadeOut().delay(4000).fadeIn();
     });
+
+
+//
+//    $('p:first').toggle(function() {
+//        $(this).animate({'height':'+=150px'}, 2000, 'linear');
+//        alert('toggled');
+//    }, function() {
+//        $(this).animate({'height':'-=150px'}, 2000, 'swing');
+////        alert('toggled again');
+//    });
+//
+
+
+    $('p:first').animate({height: '+=300px'}, 2000, 'easeOutBounce');
+    $('p:first').animate({height: '-=300px'}, 2000, 'easeInOutExpo');
+    $('p:first').animate({height: 'hide'}, 2000, 'easeOutCirc');
+    $('p:first').animate({height: 'show'}, 2000, 'easeOutElastic');
+
+    $('<div id="navigation_blob"></div>').css({
+        width: 0,
+        height: $('#navbarResponsive li:first a').height() + 10
+    }).appendTo('#navbarResponsive');
+
+
+    $('#navbarResponsive a').hover(function() {
+        // Mouseover function
+        $('navigation_blob').animate({
+            width: $(this).width() + 10,
+            left: $(this).position().left
+        },{
+            duration: 'slow',
+            easing: 'easeOutCirc',
+            queue: false
+        });
+    }, function() {
+        // Mouseout function
+        $('navigation_blob').animate({
+            width: $(this).width() + 10,
+            left: $(this).position().left
+        },{
+            duration: 'slow',
+            easing: 'easeOutCirc',
+            queue: false
+        }).animate({
+            left: $('navbarResponsive li:first a').position().left
+        }, 'fast');
+    });
+
 
 });
